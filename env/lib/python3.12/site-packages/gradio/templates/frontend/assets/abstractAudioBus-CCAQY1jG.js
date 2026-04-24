5a8d4ef9d05ce271e3d7621f6233182ffa30998d
shreyshringare <shreyas.shringare@gmail.com>
2026-04-24 08:08:14 +0000

    Remove virtual environment from repo

--- env/lib/python3.12/site-packages/gradio/templates/frontend/assets/abstractAudioBus-CCAQY1jG.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./webAudioBaseSubGraph-CNNuTFUE.js";class A extends o{constructor(s,t){super(s,t,3)}}export{A};
--- env/lib/python3.12/site-packages/gradio/templates/frontend/assets/color-DNxsOKs1.js
+++
@@ -1,1 +0,0 @@
-import{o}from"./i18n-Dnhn5Rxr.js";const t=r=>o[r%o.length];export{t as g};
